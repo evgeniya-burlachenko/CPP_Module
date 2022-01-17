9061badaf3ffa9cec4be24ecec7afeaad7836676
evgeniya-burlachenko <e.accounted@gmail.com>
2022-01-18 00:39:22 +0300

    ex01

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -10,17 +10,15 @@ int main(int argc, char **argv)
 	else
 	{
 		i = 1;
-		while(i < argc)
+		for(i = 1; i < argc; i++)
 		{
-			j = 0;
-			while(argv[i][j] != '\0')
+			j = -1;
+			while(argv[i][++j] != '\0')
 			{
 				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
 					argv[i][j] -= 32;
-				j++;
 			}
 			std::cout << argv[i];
-			i++;
 		}
 		std::cout << std::endl;
 	}	
