768b27fe8f853c5783e54e6b8c0f423f26db87d5
Evgeniya Burlachenko <63720882+evgeniya-burlachenko@users.noreply.github.com>
2022-01-18 00:10:39 +0300

    Update megaphone.cpp

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -9,18 +9,16 @@ int main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		i = 1;
-		while(i < argc)
+		for(int i = 1; i < argc; i++)
 		{
-			j = 0;
-			while(argv[i][j] != '\0')
+			j = -1;
+			while(argv[i][++j] != '\0')
 			{
 				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
 					argv[i][j] -= 32;
 				j++;
 			}
 			std::cout << argv[i];
-			i++;
 		}
 		std::cout << std::endl;
 	}	
