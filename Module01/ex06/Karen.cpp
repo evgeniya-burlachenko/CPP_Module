0df6e3d23d7d30c9569c4f9a43440ea6303fad1a
evgeniya-burlachenko <e.accounted@gmail.com>
2022-01-21 23:50:34 +0300

    ex06

---
+++ Module01/ex06/Karen.cpp
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skelly <skelly@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 20:28:44 by skelly            #+#    #+#             */
+/*   Updated: 2022/01/21 23:48:25 by skelly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Karen.hpp"
+
+Karen::Karen(void)
+{
+	return ;
+}
+Karen::~Karen(void)
+{
+	return ;
+}
+
+void Karen::debug(void)
+{	
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love to get extra baconfor my "
+				"7XL-double-cheese-triple-pickle-special-ketchup "
+				"burger. I just love it!" << std::endl;
+	std::cout << std::endl;
+}
+void Karen::info(void)
+{
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extrabacon "
+				"cost more money. You don’t put enough! "
+				"If you did I would not have to askfor it!" << std::endl;
+	std::cout << std::endl;
+}
+void Karen::warning(void)
+{
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve beencoming here for years and you just started working here last month." << std::endl;
+	std::cout << std::endl;
+}
+void Karen::error(void)
+{
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+	std::cout << std::endl;
+}
+
+void Karen::invalid_level(void)
+{
+	std::cout << "[ Probably complaining about insignificant problems ]" <<std::endl;
+}
+
+void Karen::complain(std::string level)
+{
+	std::string array[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	//массив указателей на ф-ции
+	void	(Karen::*func[5])(void) = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error, &Karen::invalid_level};
+	int i = 0;
+	//иду по массиву и сравниваю со строкой, которую передали
+	while (i < 4 && level != array[i])
+		i++;
+	//если находит совпадение выходит из массива и заходит в другой массив, которая вызывает ф-цию по найденному индексу 
+	switch (i)
+	{
+		case 0:
+			(this->*func[0])();
+		case 1:
+			(this->*func[1])();
+		case 2:
+			(this->*func[2])();
+		case 3:
+			(this->*func[3])();
+			break ;
+		default:
+			(this->*func[4])();	
+	}
+}
---
+++ Module01/ex06/Karen.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skelly <skelly@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 20:20:30 by skelly            #+#    #+#             */
+/*   Updated: 2022/01/21 23:31:26 by skelly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KAREN_HPP
+# define KAREN_HPP
+
+#include <iostream>
+
+class Karen
+{
+	//4 метода или левела
+	void debug(void);
+	void info(void);
+	void warning(void);
+	void error(void);
+	void invalid_level(void);
+
+	public:
+		Karen(void);
+		~Karen(void);
+		void complain(std::string level);
+};
+
+#endif
